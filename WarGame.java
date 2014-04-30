1c8d0d1db28390dea49325055e45ef2652221038
aziminnsk <aziminsk@uvm.edu>
2014-04-29 21:40:54 -0400

    finished ruleset
    Finished the rules and game class

--- WarGame.java
+++ WarGame.java
@@ -1,4 +1,179 @@
+import java.util.Scanner;
+
 public class WarGame
 {
-
+   int weiner;
+   WarRules rules = new WarRules();
+   
+   /**
+      Default constructor. Initiates the creatDecks method and
+      prepares them for the game
+   */
+   
+   public WarGame()
+   {
+      rules.createDecks();
+   }
+   
+   /**
+      This method is used to process a turn and determine who
+      won and add cards to the correct deck.
+      @param gameOverMan checks whether or not the game is over
+      @param equaled checks the cards are equal
+      @param greater checks to see if the player 1 card is higher than player 2's card
+      @param turnWinner int used to tell who won
+   */
+   
+   public int turn()
+   {  
+      boolean gameOverMan = rules.isEmpty();
+      boolean equaled;
+      boolean greater;
+      int turnWinner = 0;
+         
+      if (gameOverMan == true)
+      {
+         weiner = rules.findWinner();
+         if (weiner == 1)
+         {
+            System.out.println("Player 1 wins!");
+         }
+         else if (weiner == 2)
+         {
+            System.out.println("Player 2 wins!");
+         }
+         else
+         {
+            System.out.println("Error.");
+         }
+      }
+      else
+      {
+         rules.p1Turn();
+         rules.p2Turn();
+         
+         Card rightCard = new Card();
+         Card leftCard = new Card();
+         
+         if(rules.p1DeckSize() == 0)
+         {
+            System.out.println("Player 2 wins");
+         }
+         else
+         {
+            leftCard = rules.getP1TopCard();
+         }
+         
+         if(rules.p2DeckSize() == 0)
+         {
+            System.out.println("Player 1 wins");
+         }
+         else
+         {         
+            rightCard = rules.getP2TopCard();
+         }
+         
+         equaled = leftCard.equals(rightCard);
+         greater = leftCard.greaterThan(rightCard);
+         
+         if (equaled == true)
+         {
+            rules.returnCards();
+            int champion = rules.weGotAWar();
+            
+            if(champion == 1)
+            {
+               turnWinner = 1;
+               System.out.println("Player 1 won the war.");
+            }
+            else if (champion == 2)
+            {
+               turnWinner = 2;
+               System.out.println("Player 2 won the war.");
+            }
+         }
+         else
+         {
+            if(greater == true)
+            {
+               turnWinner = 1;
+               rules.p1Win();
+               System.out.println("player 1 won that round.");
+            }
+            else
+            {
+               turnWinner = 2;
+               rules.p2Win();
+               System.out.println("player 2 won that round.");
+            }
+         }
+         
+      }
+      
+      return turnWinner;  
+      
+   }
+   
+   /**
+      This method finds the size of the
+      deck and returns it
+      @param size the int value for the size of the deck
+      @return size the int
+   */
+   
+   public int p1DeckSize()
+   {
+      int size = rules.p1DeckSize();
+      return size;
+   }
+   
+   /**
+      This method finds the size of the
+      deck and returns it
+      @param size the int value for the size of the deck
+      @return size the int
+   */
+   
+   public int p2DeckSize()
+   {
+      int size = rules.p2DeckSize();
+      return size;
+   }
+   
+   /**
+      This method finds the top card and prints it
+      @param combatant the top card being played
+   */
+   
+   public void p1Played()
+   {
+      Card combatant = rules.getP1TopCard();
+      System.out.println(combatant);
+   }
+   
+   /**
+      This method finds the top card and prints it
+      @param combatant the top card being played
+   */
+   
+   public void p2Played()
+   {
+      Card combatant = rules.getP2TopCard();
+      System.out.println(combatant);
+   }
+   
+   public static void main(String [] args)
+   {
+      WarGame game = new WarGame();
+      Scanner keyboard = new Scanner(System.in);
+      String blank = "y";
+      while (blank != "n")
+      {
+         game.turn();
+         System.out.println(game.p1DeckSize());
+         System.out.println(game.p2DeckSize());
+         System.out.println("press enter for next turn or type 'n'");
+         blank = keyboard.nextLine();
+      }
+   }
 }
--- WarRules.java
+++ WarRules.java
@@ -17,6 +17,8 @@ public class WarRules extends Card
 {
    private final ArrayList<Card> p1 = new ArrayList<Card>();
    private final ArrayList<Card> p2 = new ArrayList<Card>();
+   private final ArrayList<Card> p1Turn = new ArrayList<Card>();
+   private final ArrayList<Card> p2Turn = new ArrayList<Card>();
    final int FULLDECK = 52;
    final int HALFDECK = 26;
    int theWinner = 0;
@@ -74,7 +76,17 @@ public class WarRules extends Card
    
    public Card getP1TopCard()
    {
-      Card card = p1.get(0);
+      Card card;
+      
+      if(p1Turn.size() == 0)
+      {
+         card = null;
+      }
+      else
+      {
+         card = p1Turn.get(0);
+      }
+
       return card;
    }
    
@@ -86,7 +98,14 @@ public class WarRules extends Card
    
    public Card getP2TopCard()
    {
-      Card card = p2.get(0);
+      Card card;
+      
+      if (p2Turn.size() == 0)
+      {
+         card = null;
+      }
+      else
+         card = p2Turn.get(0);
       return card;
    }
    
@@ -113,33 +132,64 @@ public class WarRules extends Card
       ArrayList<Card> war1 = new ArrayList<Card>();
       ArrayList<Card> war2 = new ArrayList<Card>();
       
-      for (int i = 0; i <= 4; i++)
+      if (p1.size() > 3)
       {
-         Card scard1 = p1.get(i);
-         p1.remove(i);
-         Card scard2 = p2.get(i);
-         p2.remove(i);
-         
-         war1.add(scard1);
-         war2.add(scard2);
+         for (int i = 0; i < 4; i++)
+         {
+            Card scard1 = p1.get(i);
+            p1.remove(i);
+            
+            war1.add(scard1);
+         }
+      }
+      else
+      {
+         for (int i = 0; i < p1.size(); i++)
+         {
+            Card scard1 = p1.get(i);
+            p1.remove(i);
+            
+            war1.add(scard1);
+         }
       }
       
-      Card gun1 = war1.get(3);
-      Card gun2 = war2.get(3);
+      if (p2.size() > 3)
+      {
+         for (int i = 0; i < 4; i++)
+         {
+            Card scard2 = p2.get(i);
+            p2.remove(i);
+            
+            war2.add(scard2);
+         }
+      }
+      else
+      {
+         for (int i = 0; i < p2.size(); i++)
+         {
+            Card scard2 = p2.get(i);
+            p2.remove(i);
+            
+            war2.add(scard2);
+         }
+      }
+      
+      Card gun1 = war1.get(0);
+      Card gun2 = war2.get(0);
       
       boolean combat = gun1.greaterThan(gun2);
       
       if (combat == true)
       {
          winner = 1;
-         for (int i = 0; i < war1.size(); i++)
+         for (int i = 0; i < war1.size();)
          {
             Card prize = war1.get(i);
             p1.add(prize);
             war1.remove(i);
          }
          
-         for (int i = 0; i < war2.size(); i++)
+         for (int i = 0; i < war2.size();)
          {
             Card prize = war2.get(i);
             p1.add(prize);
@@ -149,14 +199,14 @@ public class WarRules extends Card
       else
       {
          winner = 2;
-         for (int i = 0; i < war1.size(); i++)
+         for (int i = 0; i < war1.size();)
          {
             Card prize = war1.get(i);
             p2.add(prize);
             war1.remove(i);
          }
          
-         for (int i = 0; i < war2.size(); i++)
+         for (int i = 0; i < war2.size();)
          {
             Card prize = war2.get(i);
             p2.add(prize);
@@ -181,6 +231,7 @@ public class WarRules extends Card
       if (p1.size() == 0 || p2.size() == 0)
       {
          gameOver = true;
+         
          if (p1.size() == 0)
          {
             theWinner = 1;
@@ -197,7 +248,162 @@ public class WarRules extends Card
       
       return gameOver;
    }
-       
+   
+   /**
+      This method returns the winner of the
+      game if there is one
+      @return theWinner int representing winner of the game.
+   */
+   
+   public int findWinner()
+   {
+      return theWinner;
+   }
+   
+   /**
+      This method takes two cards
+      @param turnCard the card being used in the turn
+   */
+   
+   public void p1Turn()
+   {
+      if (p1.size() > 2)
+      {
+         for(int i = 0; i < 2; i++)
+         {
+            Card turnCard = p1.get(i);
+            p1Turn.add(turnCard);
+            p1.remove(i);
+         }
+      }
+      else
+      {
+         Card turnCard = p1.get(0);
+         p1Turn.add(turnCard);
+         p1.remove(0);
+      }
+      
+   }
+   
+   /**
+      This method takes two cards
+      @param turnCard is the card that are being used in the turn
+   */
+   
+   public void p2Turn()
+   {
+      if (p2.size() > 2)
+      {
+         for(int i = 0; i < 2; i++)
+         {
+            Card turnCard = p2.get(i);
+            p2Turn.add(turnCard);
+            p2.remove(i);
+         }
+      }
+      else
+      {
+         Card turnCard = p2.get(0);
+         p1Turn.add(turnCard);
+         p2.remove(0);
+      }
+   }
+   
+   /**
+      This method takes the cards from the turn
+      arrays and adds them to the first
+      player's deck
+   */
+   
+   public void p1Win()
+   {
+      for(int i = 0; i < p1Turn.size();)
+      {
+         Card turnCard = p1Turn.get(i);
+         p1.add(turnCard);
+         p1Turn.remove(i);
+      }
+      
+      for(int i = 0; i < p2Turn.size();)
+      {
+         Card turnCard = p2Turn.get(i);
+         p1.add(turnCard);
+         p2Turn.remove(i);
+      }
+   }
+   
+   /**
+      This method takes the cards from the turn
+      arrays and adds them to the second
+      player's deck
+   */
+   
+   public void p2Win()
+   {
+      for(int i = 0; i < p1Turn.size();)
+      {
+         Card turnCard = p1Turn.get(i);
+         p2.add(turnCard);
+         p1Turn.remove(i);
+      }
+      
+      for(int i = 0; i < p2Turn.size();)
+      {
+         Card turnCard = p2Turn.get(i);
+         p2.add(turnCard);
+         p2Turn.remove(i);
+      }
+   }
+   
+   /**
+      This method finds the size of the
+      deck and returns it
+      @param size the int value for the size of the deck
+      @return size the int
+   */
+   
+   public int p1DeckSize()
+   {
+      int size = p1.size();
+      return size;
+   }
+   
+   /**
+      This method finds the size of the
+      deck and returns it
+      @param size the int value for the size of the deck
+      @return size the int
+   */
+   
+   public int p2DeckSize()
+   {
+      int size = p2.size();
+      return size;
+   }
+   
+   /**
+      This method returns the cards that was taken
+      and returns them to their decks
+      @param badCard the cards from being played
+   */
+   
+   public void returnCards()
+   {
+      for (int i = 0; i < p1Turn.size();)
+      {
+         Card badCard = p1Turn.get(i);
+         p1.add(badCard);
+         p1Turn.remove(i);
+      }
+      
+      for (int i = 0; i < p2Turn.size();)
+      {
+         Card badCard = p2Turn.get(i);
+         p2.add(badCard);
+         p2Turn.remove(i);
+      }
+   }
+      
    public static void main(String [] args)
    {
       WarRules war = new WarRules();
